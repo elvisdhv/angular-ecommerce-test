0f353e6b82316d295347861193ceaa0788da3a87
Elvis Hernández <davidvalle.rst@gmail.com>
2025-12-11 22:59:04 -0500

    creado componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { StoreApiService } from './services/store-api-service.service';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
